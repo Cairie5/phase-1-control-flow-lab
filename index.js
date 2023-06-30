53c9890d8d129b89586f1a12acb64b8de90be0de
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-06-30 12:35:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+const {reporters} = require("mocha")
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    if (feet<=400)
+  return "This one is on me!"
+  else if (feet >= 2000 && feet <= 2500)
+  return 'I will gladly take your thirty bucks.'
+  else if (feet > 2500)
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city == "NYC" ? "Ok, soungs good." :"No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    break;
+    case "not as generous":
+      return "Thank you.";
+    break;
+    default:
+      return "Bye.";
+  }
 }
